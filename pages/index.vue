5989fe690532961de1dfa32d32d00109f89faaa6
o-xian999 <onoyama1999910@gmail.com>
2020-10-31 00:07:17 +0900

    Edit index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        NuxtFirebase
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">NuxtFirebase</h1>
     </div>
   </div>
 </template>
