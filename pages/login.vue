1b5ead1dbcf7af9f382affe25b34272ef5f82408
o-xian999 <onoyama1999910@gmail.com>
2020-10-31 16:25:13 +0900

    Add firebase to login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -15,6 +15,8 @@
 </template>
 
 <script>
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
   export default {
     data(){
       return {
@@ -25,7 +27,13 @@
     },
     methods: {
       pressed(){
-        alert('pressed')
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(this.email, this.password)
+          .then(data => {
+            console.log(data);
+            this.$router.push('/secret')
+          }).catch(error => this.error = error)
       }
     }
   }
